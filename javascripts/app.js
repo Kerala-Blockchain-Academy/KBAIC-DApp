4c04cb7e83a12fff3983bee0b299d97d5717b00d
Ananthan <ananthan.rajasekharan@gmail.com>
2022-06-08 13:25:32 +0530

    ropsten deployment

--- javascripts/app.js
+++ javascripts/app.js
@@ -27,8 +27,94 @@ $(document).ready(async function () {
     web3 = new Web3(ethereum);
     console.log("Connection Object: ", web3)
 
-    const contractAddress = MyContractJSON.networks['5777'].address;
-    const contractAbi = MyContractJSON.abi;
+    const contractAddress = "0x9Dc442323EbA915a366B4e11f87155dFCB8FA286";
+    const contractAbi = [
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [],
+            "name": "certificateCount",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "certificateDetails",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "certificateId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "courseName",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "candidateName",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "grade",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "date",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_courseName",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_candidateName",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_grade",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_date",
+                    "type": "string"
+                }
+            ],
+            "name": "newCertificate",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ];
 
     console.log("HAi", contractAbi)
 
